4ed28b595c5e4d86154c7d46a10312d5307eee32
Marabattini Sumana <suuchuu98@gmail.com>
2026-09-09 08:02:11 +0530

    Delete Downloads/COGNIS-main-2-2 directory

--- Downloads/COGNIS-main-2-2/src/lib/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr'
-import { getPublicSupabaseUrl, getPublicSupabaseAnonKey } from '@/lib/env'
-
-export function supabaseBrowser() {
-  return createBrowserClient(getPublicSupabaseUrl(), getPublicSupabaseAnonKey())
-}
